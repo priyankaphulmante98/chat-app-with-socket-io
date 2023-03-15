705014e1b2c97e26555e437248e96ae423d26cd7
priyanka <priyankaphulmante98@gmail.com>
2023-03-15 15:37:11 +0530

    lecture-5

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express') 
+const dotenv = require('dotenv').config
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running sucessfully')
+})
+
+
+app.get('/api/chats', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chats/:id', (req, res) => {
+    // res.send(req)
+    const singleChat = chats.find((c) =>c.id === req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, () => {
+    console.log('server is started on port 5000')
+})
